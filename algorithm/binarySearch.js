a6e9c612051771f9300d1b4a451975db9ccf85dc
Girish Br <girishpacchu1427@gmail.com>
2019-12-19 09:59:07 +0530

    [ANJALI] Refactor some programs

--- algorithm/binarySearch.js
+++ algorithm/binarySearch.js
@@ -1,3 +1,19 @@
+/******************************************************************************
+* Execution     : 1. default node cmd> node binarySearch.js
+*                 2. if nodemon installed cmd> nodemon binarySearch.js
+* 
+* Purpose       : Find the word in word list by using Binary Search.
+* 
+* @description 
+* 
+* @file         : binarySearch.js
+* @overview     : Binary Search program to find Word from Word List
+* @module       : binarySearch - This is optional if expeclictly its an npm or local package
+* @author       : Anjali<anjali.march19@gmail.com>
+* @version      : 1.0
+* @since        : 13-11-2019
+*
+******************************************************************************/
 let readline=require("readline-sync");
 let util=require("../utility/algorithmUtility");
 let stringArr=["d","s","a","r","j"];
