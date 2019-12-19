1e83b660043f12665cf0649759900abc010c0d97
Girish Br <girishpacchu1427@gmail.com>
2019-12-19 17:58:34 +0530

    [ANJALI] Refactor some programs

--- algorithm/message.js
+++ algorithm/message.js
@@ -8,40 +8,24 @@
 * 
 * @file     : message.js
 * @overview : Customize Message Demonstration using String Function and RegEx
-* @module   : permutation- This is optional if expeclictly its an npm or local package
+* @module   : message- This is optional if expeclictly its an npm or local package
 * @author   : Anjali<anjali.march19@gmail.com>
 * @version  : 1.0
 * @since    : 13-11-2019
 *
 ******************************************************************************/
 let readline = require("readline-sync");
+let util = require("../utility/algorithmUtility");
 let firstName, lastName, fullName, contactNumber, date;
 let message = "Hello <<name>>, We have your full name as <<full name>> in our system."
     + "Your contact number is 91-XXXXXXXXXX. Please, let us know in case of any clarification."
     + "Thank you BridgeLabz 01/01/2016.";
-getDetails();
-function getDetails() {
-    try {
-        firstName = readline.question("Enter Your First Name : ")
-        lastName = readline.question("Enter Your Last Name : ")
-        fullName = firstName + ' ' + lastName;
-        contactNumber = readline.question("Enter contact number : ")
-        date = readline.question("Enter date in dd/mm/yyyy format: ")
-        replace("<<name>>", firstName);
-        replace("<<full name>>", fullName);
-        replace("XXXXXXXXXX", contactNumber);
-        replace("01/01/2016", date);
-        console.log(message);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-function replace(replaceString, replaceStringValue) {
-    try {
-        message = message.replace(replaceString, replaceStringValue);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+firstName = readline.question("Enter Your First Name : ")
+lastName = readline.question("Enter Your Last Name : ")
+fullName = firstName + ' ' + lastName;
+contactNumber = readline.question("Enter contact number : ")
+date = readline.question("Enter date in dd/mm/yyyy format: ")
+util.data(message, firstName, lastName, fullName, contactNumber, date);
+
+
+
--- utility/algorithmUtility.js
+++ utility/algorithmUtility.js
@@ -63,9 +63,9 @@ exports.sort = (arr) => {
         }
         return arr;
     }
-    /**
-     * Program to find Word from Word List using Binary Search.
-     */
+/**
+ * Program to find Word from Word List using Binary Search.
+ */
 
 exports.binary = (stringArr, searchword) => {
     /**
@@ -74,66 +74,63 @@ exports.binary = (stringArr, searchword) => {
      * Use Arrays to sort the word list and then do the binary search
      * Print the result if the word is found
      */
-    let start=0;
-    let end=stringArr.length-1;
-    while(start<=end)
-    {
-        let mid=(start+end)/2;
-        if(searchword==stringArr[mid])
-        {
+    let start = 0;
+    let end = stringArr.length - 1;
+    while (start <= end) {
+        let mid = (start + end) / 2;
+        if (searchword == stringArr[mid]) {
             return mid;
         }
-        else if(searchword.localeCompare(stringArr[mid]<0))
-        {
-            end=mid-1;
+        else if (searchword.localeCompare(stringArr[mid] < 0)) {
+            end = mid - 1;
         }
-        else{
-            start=mid+1;
+        else {
+            start = mid + 1;
         }
     }
     return -1;
 }
 /**
- * 
+ * find the Prime numbers
  */
-exports.permutationIteration=(s)=>
-{
-    let myList=new Array();
-    myList.add(String.valueOf(s.charAt(0)));
-    for(let i=1;i<s.length();i++)
-    {
-        for(let j=myList.size()-1;j>=0;j--)
-        {
-            let str=myList.remove(j);
-            for(let k=0;k<=str.length();k++)
-            {
-                myList.add(str.substring(0,k)+s.charAt(i)+str.substring(k));
-            }
-        }
-    }
-    console.log(myList);
-}
-/**
- * 
- */
-exports.primeNum=()=>
-{
-    let count,num;
-    for(num=0;num<=1000;num++)
-    {
-        count=0;
-        for(let i=2;i<=num/2;i++)
-        {
-            if(num%i==0)
-            {
+exports.primeNum = () => {
+    /**
+     * Taking value from 0-1000
+     * print prime numbers
+     */
+    let count, num;
+    for (num = 0; num <= 1000; num++) {
+        count = 0;
+        for (let i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
                 count++;
                 break;
             }
         }
-        if(count==0&&num!=1)
-        {
+        if (count == 0 && num != 1) {
             console.log(num);
         }
     }
 }
-
+/**
+ * Customize Message Demonstration
+ */
+exports.data = (message,firstName,lastName,fullName,contactNumber,date) => {
+    /**
+     * read in the Message
+     * Replace <<name>> by first name
+     * replace <<full name>> by user full name.
+     * replace occurance of mobile number
+     * replace date
+     */
+    try{
+    message=message.replace("<<name>>", firstName);
+    message=message.replace("<<full name>>", fullName);
+    message=message.replace("XXXXXXXXXX", contactNumber);
+    message=message.replace("01/01/2016", date);
+    console.log(message);
+}
+catch (error) {
+    console.log(error);
+}
+}
